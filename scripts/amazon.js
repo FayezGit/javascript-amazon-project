32c12b0ea72288ec18d8e85471cb2b547c93d6f0
FayezGit <muhammedfayezmh@gmail.com>
2024-05-10 10:12:15 +0530

    Move calculateCartQuantity to cart.js and import into amazon.js and checkout.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 products.forEach((product) =>  {
@@ -58,13 +58,10 @@ products.forEach((product) =>  {
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
-      
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  })
+updateCartQuantity();
 
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -96,7 +96,9 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary')
-  .innerHTML = cartSummaryHTML;
+  .innerHTML = cartSummaryHTML;  
+  
+updateCartQuantity();
 
 document.querySelectorAll('.js-delete-quantity-link')
   .forEach((link) => {
@@ -107,7 +109,18 @@ document.querySelectorAll('.js-delete-quantity-link')
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`);
       container.remove();
+
+      updateCartQuantity();
     })
-  })
+  });
+
+
+  
+  function updateCartQuantity() {
+    const cartQuantity = calculateCartQuantity();
+    document.querySelector('.js-return-to-home-link')
+      .innerHTML = `${cartQuantity} items`;
+  }
+  
 
 
