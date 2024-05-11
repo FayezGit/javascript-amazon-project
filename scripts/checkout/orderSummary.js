d7ac5def6cf5f8cf2d423b09ad1310ea63be47fb
FayezGit <muhammedfayezmh@gmail.com>
2024-05-11 20:15:16 +0530

    Updating page directly with delete button instead of DOM and Updating items{) in payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -122,10 +122,7 @@ document.querySelectorAll('.js-delete-quantity-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-
-      const container = document.querySelector(
-        `.js-cart-item-container-${productId}`);
-      container.remove();
+      renderOrderSummary();
       renderPaymentSummary();
       updateCartQuantity();
     })
